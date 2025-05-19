611b58b15927e16ea0d7b53adf894c836c955487
KhushiChauhan8 <kc6478576@gmail.com>
2025-05-19 21:30:00 +0530

    add frontend URL

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ['https://code-reviewer-frontend-61g3.vercel.app/']
+}))
 
 
 app.use(express.json())
